e614d5dba81fd218bca1d82207793432d9ecd02d
Milorine <140678248+Milorine@users.noreply.github.com>
2024-07-02 16:55:48 +0800

    capteh model

---
+++ Assets/capteh.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c96a8b28922bb8ef2cb09d03d5ae42fbdc2774364c587bf323b50d507ce34722
+size 308
--- Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Annotation/PointAnnotation.cs
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Annotation/PointAnnotation.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1eb1fdb89ba9daf0eabf53935cca142a3bfc8f74bf491be76e6255426169edc
-size 4589
+oid sha256:5b520a5077eb10bb51b930b0f4f154f55234c72fbd426ee99f35de1bed0f25cb
+size 5162
